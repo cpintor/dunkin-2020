c9732497dcd6ffe5e6d74c1434664adf6a79ed05
Cristian Pintor <cpintor@hawk.iit.edu>
2021-01-21 21:13:31 -0600

    Committing code on R file

--- dunkin.R
+++ dunkin.R
@@ -1,1 +1,16 @@
-print('Hello world')
+library(ggplot2)
+
+data <- read.csv("dunkin.csv")
+
+print(data)
+
+# save png file
+#ggsave(file="dunkin-chart.png", width=10, height=5, dpi=300)
+
+# data <- data.frame("dunkin.csv")
+
+p <- ggplot(data, aes(x=date, y=price)) +
+  #geom_bar(stat = "identity")
+  geom_point()
+
+p + guides(x = guide_axis(angle = 90))
