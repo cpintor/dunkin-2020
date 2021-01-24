f93ff20540d012923813adfcd07fc7c103488876
Cristian Pintor <cpintor@hawk.iit.edu>
2021-01-24 00:24:54 -0600

    Adding up each month's totals

--- dunkin.R
+++ dunkin.R
@@ -1,4 +1,6 @@
 library(ggplot2)
+library(dplyr)
+library(lubridate)
 
 data <- read.csv("dunkin.csv")
 data2019 <- read.csv("dunkin-2019.csv")
@@ -17,3 +19,13 @@ p <- ggplot(dataTotal, aes(x=date, y=price)) +
   geom_point()
 
 p + guides(x = guide_axis(angle = 90))
+
+# Combining by month, 2019
+byMonth2019 <- aggregate(cbind(price)~month(date),
+                     data=data2019,FUN=sum)
+
+byMonth2020 <- aggregate(cbind(price)~month(date),
+                         data = data, FUN = sum)
+
+print(byMonth2019)
+print(byMonth2020)
