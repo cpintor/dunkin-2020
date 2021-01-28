8dab534c864b01f87ddd3287f5064840f051d812
Cristian Pintor <cpintor@hawk.iit.edu>
2021-01-27 23:15:11 -0600

    Creating barplots for monthly totals

--- dunkin.R
+++ dunkin.R
@@ -29,3 +29,20 @@ byMonth2020 <- aggregate(cbind(price)~month(date),
 
 print(byMonth2019)
 print(byMonth2020)
+
+# Creating data frames for months in year
+month2019 <- c(10, 11, 12)
+total2019 <- c(29.48, 36.20, 40.49)
+
+df2019 <- data.frame(month2019, total2019)
+
+print(df2019)
+
+g <- ggplot(df2019, aes(x=month2019, y=total2019)) +
+  #geom_bar(stat = "identity")
+  geom_point()
+
+g + guides(x = guide_axis(angle = 90))
+
+month2020 <- c(1:12)
+total2020 <- c(30.54, 24.81, 36.29, 38.19, 38.43, 40.06, 55.99, 31.55, 49.74, 55.16, 46.86, 37.19)
